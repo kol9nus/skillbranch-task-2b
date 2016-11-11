3e6cdc13d20fb084906ebe8fd1e5c0302d946200
Nikolai Bravilov <bravilov@mail.ru>
2016-11-11 13:57:28 +0500

    Imlement formatting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 import express from 'express';
 import cors from 'cors';
 
+const onlyLettersPattern = /^[^\.,\\/#!$%^&*;:{}=\-_`~()0-9]*$/i;
+
+function firstLetterToUpper(word) {
+  return `${word[0].toUpperCase()}${word.substring(1)}`;
+}
+
+function splitFullName(fullName) {
+  if (!fullName) {
+    return [];
+  }
+
+  const splittedName = fullName.trim()
+    .toLowerCase()
+    .replace(/\s+/g, ' ')
+    .split(' ')
+    .map(firstLetterToUpper);
+
+  if (splittedName.length > 3 || splittedName.length === 0) {
+    return [];
+  }
+
+  const result = [splittedName[splittedName.length - 1]];
+  if (splittedName.length > 1) {
+    result.push(splittedName[0]);
+    if (splittedName.length > 2) {
+      result.push(splittedName[1]);
+    }
+  }
+  return result;
+}
+
+function getInitial(name) {
+  return name ? ` ${name[0]}.` : '';
+}
+
+function formatName(lastName, firstName, middleName) {
+  const middleNameInitial = getInitial(middleName);
+  const firstNameInitial = getInitial(firstName);
+
+  return `${lastName}${firstNameInitial}${middleNameInitial}`;
+}
+
 const app = express();
 app.use(cors());
 app.get('/', (req, res) => {
-  res.json({
-    hello: 'JS World',
-  });
+  const splittedName = splitFullName(req.query.fullname);
+  const isEveryNameCorrect = splittedName.every(name => onlyLettersPattern.test(name));
+  const formattedName = splittedName.length && isEveryNameCorrect ?
+    formatName(...splittedName) : 'Invalid fullname';
+  res.send(formattedName);
 });
 
 app.listen(3000, () => {
